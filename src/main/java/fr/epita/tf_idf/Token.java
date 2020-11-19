9b1f25f197808d140648f7a0fe5501fd0a2e7157
Ingo Braekman <ingo.braekman@epita.fr>
2020-11-19 15:18:26 +0100

    VectorizationLayer and Token

--- src/main/java/fr/epita/tf_idf/Token.java
+++ src/main/java/fr/epita/tf_idf/Token.java
@@ -1,4 +1,34 @@
 package fr.epita.tf_idf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Token {
+    final String word;
+    float frequency;
+    final List<Integer> indexes;
+
+    public Token(String word, float frequency, List<Integer> indexes) {
+        this.word = word;
+        this.frequency = frequency;
+        this.indexes = indexes;
+    }
+
+    public Token(String word, float frequency, int index) {
+        this.word = word;
+        this.frequency = frequency;
+        indexes = new ArrayList<>(index);
+    }
+
+    public void addIndex(int index) {
+        indexes.add(index);
+    }
+
+    public void setFrequency(float frequency) {
+        this.frequency = frequency;
+    }
+
+    public int getCount() {
+        return indexes.size();
+    }
 }
--- src/main/java/fr/epita/tf_idf/VectorizationLayer.java
+++ src/main/java/fr/epita/tf_idf/VectorizationLayer.java
@@ -1,9 +1,22 @@
 package fr.epita.tf_idf;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class VectorizationLayer {
-    public ArrayList<Token> vectorize(ArrayList<String> tokenListFromRawHTML) {
-        return new ArrayList<>();
+    public ArrayList<Token> vectorize(List<String> tokenListFromRawHTML) {
+        ArrayList<Token> vector = new ArrayList<>();
+        for (int i = 0; i < tokenListFromRawHTML.size(); i++) {
+            String word = tokenListFromRawHTML.get(i);
+            Optional<Token> token = vector.stream().filter(x -> x.word.equals(word)).findFirst();
+            if (token.isEmpty())
+                vector.add(new Token(word, 1f / tokenListFromRawHTML.size(), i));
+            else {
+                token.get().addIndex(i);
+                token.get().setFrequency(((float)token.get().getCount()) / tokenListFromRawHTML.size());
+            }
+        }
+        return vector;
     }
 }
