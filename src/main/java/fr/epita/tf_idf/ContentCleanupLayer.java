21761dc771fe35f877188a26748b0fa9cbcafecc
Doomky <james.richet.pro@gmail.com>
2020-11-19 15:04:29 +0100

    [*] ContentCleanupLayer : implemented

--- src/main/java/fr/epita/tf_idf/ContentCleanupLayer.java
+++ src/main/java/fr/epita/tf_idf/ContentCleanupLayer.java
@@ -1,7 +1,11 @@
 package fr.epita.tf_idf;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class ContentCleanupLayer {
-    public String cleanupHTML(String htmlFromURL) {
-        return "";
+    public String cleanupHTML(String html) {
+        Document doc = Jsoup.parse(html);
+        return doc.text();
     }
 }
