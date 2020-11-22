3d444e7187d0b0191e9c017a09ea62cff51a1349
Allan Cantin <allan.cantin@epita.fr>
2020-11-22 19:22:14 +0100

    [Token] fixed tests

--- src/test/java/fr/epita/tf_idf/TokenizationLayerTest.java
+++ src/test/java/fr/epita/tf_idf/TokenizationLayerTest.java
@@ -21,8 +21,8 @@ class TokenizationLayerTest {
     public void TestTokenization() {
         String raw = "Bonjour, je m'appelle Allan.";
         List<String> tokenList = tokenizationLayer.tokenize(raw);
-        //Assertions.assertTrue(tokenList.contains("bonjour"));
-        //Assertions.assertTrue(tokenList.contains("appelle"));
+        Assertions.assertTrue(tokenList.contains("bonjour"));
+        //Assertions.assertTrue(tokenList.contains("appell"));
         Assertions.assertTrue(tokenList.contains("allan"));
     }
 
@@ -31,11 +31,11 @@ class TokenizationLayerTest {
         String raw = "Endal, né en décembre 1995 et mort le 13 mars 2009, est un chien d'assistance qui a vécu au Royaume-Uni. Adopté par Allen Parton, ancien militaire handicapé à la suite d'un accident d'automobile, ce retriever du Labrador a plusieurs exploits à son actif que les médias britanniques ont soulignés, comme utiliser un guichet automatique bancaire, charger un lave-linge et porter secours à son maître inconscient.";
         List<String> tokenList = tokenizationLayer.tokenize(raw);
         Assertions.assertTrue(tokenList.contains("1995"));
-        Assertions.assertTrue(tokenList.contains("décembre"));
+        Assertions.assertTrue(tokenList.contains("décembr"));
         Assertions.assertTrue(tokenList.contains("13"));
         Assertions.assertTrue(tokenList.contains("2009"));
-        //Assertions.assertTrue(tokenList.contains("vécu"));
-        //Assertions.assertTrue(tokenList.contains("uni"));
-        //Assertions.assertTrue(tokenList.contains("maître"));
+        Assertions.assertTrue(tokenList.contains("vécu"));
+        Assertions.assertTrue(tokenList.contains("un"));
+        Assertions.assertTrue(tokenList.contains("maîtr"));
     }
 }
